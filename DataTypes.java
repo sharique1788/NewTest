a3f2f6349270845090be4427c400de38c7c04ace
sharique1788 <sharique1788@gmail.com>
2022-09-29 06:58:48 +0530

    second command

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+package Test;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+	int a;
+	
+	int b=10;
+	a=20;
+	char c='%';
+	float f=34.23f;//float is for 2 digits decimal
+	double d=34.2335;//double is for more than 2 digits decimal
+	String st="test";
+	System.out.print("Value of f is "+f);
+	System.out.println(" and value of d is "+d);
+	System.out.println("Value of a is "+a);
+	System.out.println("Value of b is "+b);
+	System.out.println(a+b);
+	System.out.println(st+a+b);
+	System.out.println(c);
+	}
+
+}
